b852f005ab5ee41c20a380c93c90bb83575ca07b
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-19 16:19:33 +0200

    Setting up react router to work with gh-pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import createHashHistory from 'history/createHashHistory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render( <Router history={hashHistory}>
+								<App />
+								</Router>, document.getElementById('root'));
 registerServiceWorker();
