3841536d2449be0be5cc533e3f301ff4dbad84e4
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-19 16:21:08 +0200

    Added forgotten Router to index.is file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router } from 'react-router';
 import createHashHistory from 'history/createHashHistory';
 
 const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
