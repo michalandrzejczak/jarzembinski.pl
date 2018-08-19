cb03179e6fca362a3c5ae641619258ef33402be9
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-19 16:01:07 +0200

    setting up dev branch

---
+++ src/Components/Kontakt.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Kontakt extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Kontakt</h2>
+				<pre>Tutaj formularz</pre>
+			</div>
+		);
+	}
+}
+
+export default Kontakt;
