8dedd335761acd60e70c482637f1c354c5ed4c9d
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-20 21:01:40 +0200

    Added gulp setup for working with SASS

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const sass = require('gulp-ruby-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('buildcss', () =>{
+  gulp.src('./src/sass/*.scss')
+	  return sass('./src/sass/*.scss').on('error', sass.logError)
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./src'))
+});
+
+
+gulp.task('watch', () => {
+	gulp.watch('./src/sass/**', ['buildcss']);
+});
+
+gulp.task('default', ['buildcss']);
