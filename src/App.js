4a558e68098e259a9af319f7ecf31cdd832e28f7
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-23 22:31:44 +0200

    Fixed centering of main container, added footer

--- src/App.js
+++ src/App.js
@@ -39,21 +39,26 @@ class App extends Component {
 							</button>
 						</nav>
 					</header>
-					<section className="container">
-						<article>
-							<Switch>
-								<Route exact path="/" component={Home}/>
-								<Route path="/wizja" component={Wizja}/>
-								<Route path="/program" component={Program}/>
-								<Route path="/o-mnie" component={O_mnie}/>
-								<Route path="/kontakt" component={Kontakt}/>
-								<Route component={Error404} />
-							</Switch>
-						</article>
-						<aside>
-							<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
-						</aside>
-					</section>
+					<div class="wrapper">
+						<section className="container">
+							<article>
+								<Switch>
+									<Route exact path="/" component={Home}/>
+									<Route path="/wizja" component={Wizja}/>
+									<Route path="/program" component={Program}/>
+									<Route path="/o-mnie" component={O_mnie}/>
+									<Route path="/kontakt" component={Kontakt}/>
+									<Route component={Error404} />
+								</Switch>
+							</article>
+							<aside>
+								<div className="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+							</aside>
+						</section>
+					</div>
+					<footer>
+						2018 &copy;<a href="http://michalandrzejczak.com" target="_blank" rel="noopener noreferrer">michalandrzejczak.com</a>
+					</footer>
 				</main>
 			</BrowserRouter>
 		);
