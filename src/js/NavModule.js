9cc5a7b8f9f2b4aab377c04302e25a89da747b28
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-23 11:01:45 +0200

    Working navigation, finished also navigation for mobile

--- src/js/NavModule.js
+++ src/js/NavModule.js
@@ -1,15 +1,41 @@
 (function NavModule() {
 
-		const $ = require("jquery");
-		const hamburger = $(".hamburger");
-		const navigation = $("nav");
-		const navigationList = $("ul.navbar");
+	const $ = require("jquery");
+	const hamburger = $(".hamburger");
+	const navMenu = $("nav ul");
+	const navLink = $("nav ul a");
+	const container = $(".container");
+	let windowSize = window.innerWidth;
+	let scrollTarget;
 
-		// Hamburger toggle    
-		hamburger.on("click", hamburgerToggle)
-		function hamburgerToggle() {
-			hamburger.toggleClass("is-active");
-			navigationList.toggleClass("show_navigation");
-			navigation.toggleClass("nav-toggle");
+	// Hamburger toggle    
+	hamburger.on("click", hamburgerToggle)
+
+	function hamburgerToggle() {
+		hamburger.toggleClass("is-active");
+		navMenu.toggleClass("show_navigation");
+	}
+	
+	// Checking window viewport and scroll target
+	function checkWindow() {
+		windowSize = window.innerWidth;
+		if (windowSize > 768) {
+			scrollTarget = (container.offset().top-104); // 104px = margin-top + navbar height
+		} else {
+			scrollTarget = container.offset().top
 		}
+	}
+	checkWindow();
+
+	// NavLinks - close menu at click, scrolls to section
+	navLink.on('click', closeMenu)
+	
+	function closeMenu() {
+		checkWindow();
+		hamburgerToggle();
+		$("html, body").stop().animate({
+					scrollTop: scrollTarget
+			}, 1000);	
+	}
+	
 })();
