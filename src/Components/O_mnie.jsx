85b5962ec7b12b869b11c4805d9c7fc0e6b5ace7
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-08-28 21:38:41 +0200

    Added 'wizja' section

--- src/Components/O_mnie.jsx
+++ src/Components/O_mnie.jsx
@@ -5,7 +5,17 @@ class O_mnie extends Component {
 		return (
 			<div>
 				<h2>O mnie</h2>
-				<img height="200px" width="200px" src="underconstruction.gif" alt="Under construction gif" />
+				<div className="text-container">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et vitae inventore suscipit eligendi, libero cupiditate atque nisi qui, perferendis id labore fugiat mollitia facere iste voluptate. Voluptatibus iure deleniti perferendis incidunt, repudiandae facilis eaque.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quod autem unde odio consectetur dolor et ducimus placeat necessitatibus tempore iusto, dignissimos enim, nostrum maxime. Enim, architecto! Exercitationem ut similique, ducimus, iure, obcaecati laboriosam placeat magni voluptate odit quos sequi. Suscipit blanditiis quibusdam fugit, aliquam officiis sequi assumenda, inventore, numquam provident laudantium eaque deserunt sint culpa quia dolore earum! Ducimus magni repellat inventore. Repellendus veniam, aliquam similique sit nihil rerum deleniti commodi, cupiditate libero! Nostrum commodi sapiente esse ab nam architecto, aliquam, est vitae, unde iure ipsam ut delectus blanditiis numquam voluptatibus minima. Nam recusandae quidem quisquam sit, et aperiam deleniti assumenda atque vel laborum consequuntur aliquid amet quae officia minus voluptatum, harum. Recusandae at maiores adipisci consequuntur optio libero vel, quibusdam fugit. Dignissimos quidem tempora quibusdam in maxime et modi maiores est rerum, expedita dolorum ut sit fuga dolor. Consequatur impedit deleniti aliquam voluptatem ex at autem, itaque culpa!
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora fuga facere optio modi neque rem doloribus at, nihil quaerat expedita?
+					</p>
+				</div>
 			</div>
 		);
 	}
--- src/Components/Wizja.jsx
+++ src/Components/Wizja.jsx
@@ -5,7 +5,20 @@ class Wizja extends Component {
 		return (
 			<div>
 				<h2>Wizja</h2>
-				<img height="200px" width="200px" src="underconstruction.gif" alt="Under construction gif" />
+				<div className="text-container">
+					<div className="para_align-left">
+					Moja Gdynia to miasto otwarte, obywatelskie i działające na przejrzystych zasadach.
+					</div>
+					<div className="para_align-right ">
+						Miasto, które uczciwie przeprowadza konsultacje z mieszkańcami, przede wszystkim na wrażliwe i ważne dla społeczności lokalnej tematy.
+					</div>
+					<div className="para_align-left">
+						Moja Gdynia korzysta z zaangażowania osób pracujących na jej rzecz, nie zważając na partyjne i inne nieistotne podziały. 
+					</div>
+					<div className="para_align-right ">
+						Moja Gdynia to także miasto, które dba o wszystkie swoje dzielnice i swoją uwagę poświęca nie tylko projektom ogólnomiejskim, ale także służącym konkretnym dzielnicom.
+					</div>
+				</div>
 			</div>
 		);
 	}
